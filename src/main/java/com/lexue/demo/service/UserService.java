5e77c573393880a58d785e51f49426519bb3ef29
wentai <wangwentai@lexue.com>
2020-10-16 11:56:03 +0800

    1

--- src/main/java/com/lexue/demo/service/UserService.java
+++ src/main/java/com/lexue/demo/service/UserService.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 public interface UserService {
 
     @CacheRefresh(refresh = 1, timeUnit = TimeUnit.SECONDS, refreshLockTimeout = 60)
-    @Cached(name = "retcache:userCache.", key = "#userId", expire = 3600, timeUnit = TimeUnit.SECONDS, cacheType = CacheType.REMOTE)
+    @Cached(name = "retcache:userCache.", key = "#userId", expire = 3600, timeUnit = TimeUnit.SECONDS, cacheType = CacheType.LOCAL)
     @CachePenetrationProtect // 当缓存访问未命中的情况下，对并发进行的加载行为进行保护。当前版本实现的是单JVM内的保护，即同一个JVM中同一个key只有一个线程去加载，其它线程等待结果。
     User selectById(Long userId);
 
